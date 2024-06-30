0de2618574b57a7d60d25bd674c3d6580052f553
MaouOni <116459612+MaouOni@users.noreply.github.com>
2024-06-30 10:08:12 -0600

    Appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component'; // Make sure this path is correct
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { MatTableModule } from '@angular/material/table';
@@ -35,7 +35,8 @@ import { UserEditComponent } from './user-edit/user-edit.component';
     MatButtonModule,
     MatIconModule,
     HttpClientModule,
-    FormsModule, BrowserAnimationsModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
